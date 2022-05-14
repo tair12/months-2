e23e01b4538708119567dd6ca9aa2fefa03a4db2
tair12 <tair-00@list.ru>
2022-05-14 15:31:50 +0300

    hw2

---
+++ lesson 2 Tair Abdalimov 13_02/greeting.js
@@ -0,0 +1,78 @@
+// const array = [
+//     { git
+//         username: "jack"
+//         full_name: "jack Barbaro"
+//     },
+//     {
+//         username: "jack"
+//         full_name: jack Barbaro
+//     },
+//     { 
+//         username: "jack"
+//         full_name: "jack Barbaro"
+//     },
+// ]
+// for( let i = 2; i < array.length; i++){
+//     console,log(array[i])
+// }
+// for(let user of array){
+//     console.log(user)
+// }
+// const obj = {
+//     key1: "b1"
+//     key2: "b2"
+//     key3: "b3"
+//     key4: "b4"
+//     key5: "b5"
+//     key6: "b6"
+//     key7: "b7"
+//     key8: "b8"
+//     key9: "b9"
+// }
+// for(let key in obj){
+//     console.log(obj)
+// }
+// let i = 0;
+//  while( i < 5 ){
+//      console.log("hello")
+//      i++
+//  }
+//  const users = [
+//      {
+//          username: "jack",
+//          salary:5000
+//      }
+//      {
+//          username: "john",
+//          salary:500
+//      }
+//      {
+//          username: "joe",
+//          salary:10000
+//      }
+//  ]
+//  const filteredUsers = users.filter(user=> user.salary > 5000)
+//  console.log ( filtredUser)
+ 
+//  const   tests = [
+//      {
+//          username: "red",
+//          power:5000
+//      },
+//      {
+//          username: "black",
+//          power:7000
+//      },
+//      {
+//          username: "blue",
+//          power:10000
+//      },
+     
+//      {
+//          username: "green",
+//          power:15000
+//      },
+//  ]
+//  const filteredTests = tests.filter( test => test.power > 10000 )
+//  console.log( filteredTests)
+ 
